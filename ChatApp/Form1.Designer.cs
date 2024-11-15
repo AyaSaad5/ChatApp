4b74fa4387a631dafed3fcb5a66d50625624b5cb
Aya <ayasa3d5@gmail.com>
2024-11-15 14:21:50 +0200

    Add project files.

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ChatApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.join = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.mssgs = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(262, 119);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 0;
+            this.send.Text = "send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "group";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(116, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // join
+            // 
+            this.join.Location = new System.Drawing.Point(262, 162);
+            this.join.Name = "join";
+            this.join.Size = new System.Drawing.Size(75, 23);
+            this.join.TabIndex = 8;
+            this.join.Text = "join";
+            this.join.UseVisualStyleBackColor = true;
+            this.join.Click += new System.EventHandler(this.join_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(141, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "send To group";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mssgs
+            // 
+            this.mssgs.FormattingEnabled = true;
+            this.mssgs.ItemHeight = 16;
+            this.mssgs.Location = new System.Drawing.Point(41, 268);
+            this.mssgs.Name = "mssgs";
+            this.mssgs.Size = new System.Drawing.Size(328, 148);
+            this.mssgs.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mssgs);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.join);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button join;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox mssgs;
+    }
+}
+
---
+++ ChatApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatApp
+{
+    public partial class Form1 : Form
+    {
+        IHubProxy prox;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            prox.Invoke("sendMessage", textBox1.Text, textBox2.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //define connection
+            HubConnection con = new HubConnection("https://localhost:44328/");
+
+            //create proxy
+            prox = con.CreateHubProxy("chat");
+
+            //start connection
+            con.Start();
+
+            //  method for client
+            //prox.On<data>("newMessage", (m) => mssgs.Invoke( new Action(() => mssgs.Items.Add(m.name + ":" + m.message))));
+            prox.On<string, string>("newMessage", (n, m) => mssgs.Invoke(new Action(() => mssgs.Items.Add(n + ":" + m))));
+
+            prox.On<string, string>("newMember", (n, g) => mssgs.Invoke(new Action(() => mssgs.Items.Add(n + " joined " + g))));
+
+            prox.On<string, string, string>("groupmsg", (n, g, m) => mssgs.Invoke(new Action(() => mssgs.Items.Add(n + " from " + g + ": "+ m))));
+
+
+        }
+
+        private void join_Click(object sender, EventArgs e)
+        {
+            prox.Invoke("joinGroup", textBox4.Text, textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            prox.Invoke("sendToGroup", textBox4.Text, textBox1.Text, textBox2.Text);
+        }
+    }
+}
